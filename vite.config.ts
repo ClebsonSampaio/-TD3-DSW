a14cb2586a9fa3344090ac13443b645e95a4e2ea
clebsonsampaio <clebsonsampaio00@gmail.com>
2025-07-21 13:51:03 -0300

    tentando configurar deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/-TD2-DSW---Gerenciamento-Aluguel-De-Carros/", // <-- nome exato do repositório
   plugins: [react()],
-})
+});
